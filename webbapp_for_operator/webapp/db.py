a8ae443488f684172cc2898ec199d72db6403b74
Yury Borodich <xkxixnxgx@yandex.ru>
2020-09-01 16:31:09 +0300

    Вынесены ключи шифрования и доступа к удаленной базе в файл settings.py, который добавлен в .gitignore.

--- webbapp_for_operator/webapp/db.py
+++ webbapp_for_operator/webapp/db.py
@@ -1,14 +1,8 @@
 from flask_pymongo import MongoClient
+from webbapp_for_operator.settings import CONNECTION_STRING
 
 
-CONNECTION_STRING = "mongodb+srv://user1:mb_test12345@cluster0.tmwn7.mongodb.net/mb_test?retryWrites=true&w=majority"
 client = MongoClient(CONNECTION_STRING)
-# db = client['mb_test']
-# users = db["users"]
-# requests = db["requests"]
-# posts = db["posts"]
-
-# db = client.mb_test
 db = client.user_applications
 posts = db.posts
-# users = db.users
+
